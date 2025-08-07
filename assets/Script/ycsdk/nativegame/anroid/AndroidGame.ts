d8f2bc6e50416a7a5b9555041494ccb1a84f57c5
jackYan <1148791622@qq.com>
2025-08-07 10:21:51 +0800

    安卓初始化fix

--- assets/Script/ycsdk/nativegame/anroid/AndroidGame.ts
+++ assets/Script/ycsdk/nativegame/anroid/AndroidGame.ts
@@ -8,6 +8,7 @@ export class AndroidGame implements GameInterface {
     className = 'com/ycsdk/cocos/GameBridge'
 
     init(callBack?: any): void {
+        callBack && callBack()
         this.login()
     }
 
